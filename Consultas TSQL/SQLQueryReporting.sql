c365e169172fd8cdf3962009b916628a8a460724
Sneex17 <228738464+Sneex17@users.noreply.github.com>
2026-08-05 12:43:10 -0400

    Reportes de energía general para Edenorte y Edesur

--- Consultas TSQL/SQLQueryReporting.sql
+++ Consultas TSQL/SQLQueryReporting.sql
@@ -15,3 +15,27 @@ inner join DimTiempo as t on f.IdTiempo = t.IdTiempo
 inner join DimUbicacion as u on f.IdUbicacion = u.IdUbicacion
 where IdEDE = 'ED-ESTE-1' and t.Fecha between @FechaInicio and @FechaFin
 
+--Miercoles 5/8/2026
+select
+s.Nombre,
+convert(varchar,t.Fecha,103) Fecha, 
+s.Direccion,
+u.Provincia,
+f.EnergiaEntregada_GWh, f.EnergiaPerdida_GWh
+from FactEnergia as f
+inner join DimSucursal as s on f.IdSucursal = s.IdSucursal
+inner join DimTiempo as t on f.IdTiempo = t.IdTiempo
+inner join DimUbicacion as u on f.IdUbicacion = u.IdUbicacion
+where IdEDE = 'ED-SUR-2' and t.Fecha between @FechaInicio and @FechaFin
+
+select
+s.Nombre,
+convert(varchar,t.Fecha,103) Fecha, 
+s.Direccion,
+u.Provincia,
+f.EnergiaEntregada_GWh, f.EnergiaPerdida_GWh
+from FactEnergia as f
+inner join DimSucursal as s on f.IdSucursal = s.IdSucursal
+inner join DimTiempo as t on f.IdTiempo = t.IdTiempo
+inner join DimUbicacion as u on f.IdUbicacion = u.IdUbicacion
+where IdEDE = 'ED-NORTE-3' and t.Fecha between @FechaInicio and @FechaFin
