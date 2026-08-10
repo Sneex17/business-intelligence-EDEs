fa7b04979bdbd23ff37395c43462562f529b85b6
Sneex17 <228738464+Sneex17@users.noreply.github.com>
2026-08-10 09:52:15 -0400

    Cambio en la selección del los reportes

--- Consultas TSQL/SQLQueryReporting.sql
+++ Consultas TSQL/SQLQueryReporting.sql
@@ -39,3 +39,7 @@ inner join DimSucursal as s on f.IdSucursal = s.IdSucursal
 inner join DimTiempo as t on f.IdTiempo = t.IdTiempo
 inner join DimUbicacion as u on f.IdUbicacion = u.IdUbicacion
 where IdEDE = 'ED-NORTE-3' and t.Fecha between @FechaInicio and @FechaFin
+
+
+
+select IdEDE, Nombre from DimEDE
